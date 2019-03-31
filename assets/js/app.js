232d353f0f1994b545da31cd9b58fc8b1407ca45
randomslap <kentokazaki@gmail.com>
2019-03-31 00:47:38 -0700

    update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,15 @@
-var imageArr = ["Nissan", "Ford", "Mazda", "Mclaren", "Chevy"];
+var gifArr = ["Nissan", "Ford", "Mazda", "Mclaren", "Chevy"];
 function createBtn() {
 	$("#searchButtons").empty();
-	for (var i = 0; i < imageArr.length; i++) {
+	for (var i = 0; i < gifArr.length; i++) {
 		var a = $("<button>");
 		a.addClass("btn");
-		a.attr("data-input", imageArr[i]);
-		a.text(imageArr[i]);
+		a.attr("data-input", gifArr[i]);
+		a.text(gifArr[i]);
 		$("#searchButtons").append(a);
 	}
 }
-function getResults() {
+function results() {
 	var search = $(this).attr("data-input");
 	var queryURL =
 		"https://api.giphy.com/v1/gifs/search?q=" +
@@ -24,15 +24,15 @@ function getResults() {
 		var results = response.data;
 		for (var i = 0; i < results.length; i++) {
 			if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-				var gifDiv = $("<div>");
+				var div = $("<div>");
 				var rating = results[i].rating;
 				var p = $("<p>").text("Rating: " + rating);
 				var image = $("<img>");
-				image.addClass("image pause")
-                image.attr("src", results[i].images.fixed_height_still.url);
-				gifDiv.append(p);
-				gifDiv.append(image);
-				$("#searchImages").prepend(gifDiv);
+				image.addClass("image pause");
+				image.attr("src", results[i].images.fixed_height_still.url);
+				div.append(p);
+				div.append(image);
+				$("#searchImages").prepend(div);
 			}
 		}
 	});
@@ -40,27 +40,25 @@ function getResults() {
 
 $("#searchBtn").on("click", function(event) {
 	event.preventDefault();
-	var search = $("#searchText")
+	var searchTerm = $("#searchText")
 		.val()
 		.trim();
-	imageArr.push(search);
+	gifArr.push(searchTerm);
 	createBtn();
-	console.log(imageArr);
+	console.log(gifArr);
 });
 
-$(document).on("click", ".btn", getResults);
+$(document).on("click", ".btn", results);
 
 createBtn();
 
 $(document).on("click", ".image", function() {
-    var src = $(this).attr("src")
-    if ($(this).hasClass('playing')) {
-        $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
-        $(this).removeClass('playing');
-    } else {
-        //play
-        $(this).addClass('playing');
-        $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
-    }
+	var src = $(this).attr("src");
+	if ($(this).hasClass("playing")) {
+		$(this).attr("src", src.replace(/\.gif/i, "_s.gif"));
+		$(this).removeClass("playing");
+	} else {
+		$(this).addClass("playing");
+		$(this).attr("src", src.replace(/\_s.gif/i, ".gif"));
+	}
 });
-
